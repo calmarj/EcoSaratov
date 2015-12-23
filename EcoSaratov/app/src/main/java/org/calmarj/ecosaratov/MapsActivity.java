618d6ae0b369a1b400de76d370ad807fcea1b22f
calmarj <calmarj@gmail.com>
2015-12-23 20:18:52 +0300

    Created map with Saratov's marker

---
+++ EcoSaratov/app/src/main/java/org/calmarj/ecosaratov/MapsActivity.java
@@ -0,0 +1,35 @@
+package org.calmarj.ecosaratov;
+
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        LatLng saratov = new LatLng(51, 45);
+        mMap.addMarker(new MarkerOptions().position(saratov).title("Маркер в Саратове"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(saratov));
+    }
+}
